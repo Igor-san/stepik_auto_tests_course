3746a6da82a20994afe98cfe05bf06c57bca68fd
Igor <30476065+Igor-san@users.noreply.github.com>
2024-05-06 21:20:56 +0300

    Раздел 3.3 Тестирование с помощью PyTest, шаги №8-9 "PyTest — как пишут тесты" и "PyTest — проверка ожидаемого результата (assert)"

--- test_abs_project.py
+++ test_abs_project.py
@@ -7,6 +7,11 @@ class TestAbs(unittest.TestCase):
     def test_abs2(self):
         self.assertEqual(abs(-42), -42, "Should be absolute value of a number")
 
+def test_abs1():
+    assert abs(-42) == 42, "Should be absolute value of a number"
+
+def test_abs2():
+    assert abs(-42) == -42, "Should be absolute value of a number"
 
 if __name__ == "__main__":
     unittest.main()
