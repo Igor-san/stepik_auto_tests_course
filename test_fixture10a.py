df9a19e65bef85845ef67104748500a23ea47927
Igor <30476065+Igor-san@users.noreply.github.com>
2024-05-07 17:13:58 +0300

    Уроки в разделе "3.5 PyTest — маркировка"

---
+++ test_fixture10a.py
@@ -0,0 +1,38 @@
+import pytest
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+
+link = "http://selenium1py.pythonanywhere.com/"
+
+
+@pytest.fixture(scope="function")
+def browser():
+    print("\nstart browser for test..")
+    options = webdriver.ChromeOptions()
+    # It should remove all annoying errors and warnings from selenium like ERROR: Couldn't read tbsCertificate as SEQUENCE
+    options.add_experimental_option('excludeSwitches', ['enable-logging'])
+
+    browser = webdriver.Chrome(
+        service=Service(),
+        options=options,
+    )
+    yield browser
+    print("\nquit browser..")
+    browser.quit()
+
+
+class TestMainPage1():
+
+    def test_guest_should_see_login_link(self, browser):
+        browser.get(link)
+        browser.find_element(By.CSS_SELECTOR, "#login_link")
+
+    def test_guest_should_see_basket_link_on_the_main_page(self, browser):
+        browser.get(link)
+        browser.find_element(By.CSS_SELECTOR, ".basket-mini .btn-group > a")
+
+    @pytest.mark.xfail(reason="fixing this bug right now")
+    def test_guest_should_see_search_button_on_the_main_page(self, browser):
+        browser.get(link)
+        browser.find_element(By.CSS_SELECTOR, "button.favorite")
